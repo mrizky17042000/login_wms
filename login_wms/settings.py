0adeb9261e60348b0c54fdfde2241a7838b2aef5
mrizky17042000 <mriski889@gmail.com>
2020-08-21 08:08:50 +0700

    Last Deploy

--- login_wms/settings.py
+++ login_wms/settings.py
@@ -56,8 +56,13 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'login_wms.urls'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
